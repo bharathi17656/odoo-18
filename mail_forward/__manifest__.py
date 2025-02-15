1b7b8e2fd7a46a90f37a8e8ef1974ed0adcd5496
Bharathikannan M <115794225+bharathi17656@users.noreply.github.com>
2025-02-15 12:15:44 +0530

    Update __manifest__.py

--- mail_forward/__manifest__.py
+++ mail_forward/__manifest__.py
@@ -1,21 +1,17 @@
-# Copyright 2024 Tecnativa - Carlos Lopez
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 {
-    "name": "Mail Forward Message",
-    "version": "15.0.1",
-    "summary": "Forward messages from the chatter of any document to other users.",
-    "author": "Tecnativa, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/social",
+    "name": "Mail Forward Message - Odoo 18",
+    "version": "18.0.1",
+    "summary": "Forward messages from the chatter message.",
+    "author": "bharathikannan17656@gmail.com",
     "depends": ["mail", "contacts"],
     "data": [
-        # "wizards/mail_compose_message_view.xml",
+       
     ],
     "assets": {
         "web.assets_backend": [
-            "mail_forward/static/src/components/forward_message.js",
-            
-        ],
-        
+            "mail_forward/static/src/components/forward_message.js",       
+        ],  
     },
     "installable": True,
     "auto_install": False,
